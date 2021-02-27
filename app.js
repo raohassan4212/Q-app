193baa30cf7ce192a5f254d6916e8ffe0947eb9e
Rao Hassan <raohassan4212@gmail.com>
2021-02-27 08:55:03 +0500

    start javascript

--- app.js
+++ app.js
@@ -60,37 +60,44 @@ let questions = [
     },
 ]
 
-const start_btn = document.querySelector(".start_btn button");
-const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart");
-const quiz_box = document.querySelector(".quiz_box");
 
 
-start_btn.onclick = ()=> {
-    info_box.classList.add('activeInfo');
-}
 
-exit_btn.onclick = ()=> {
-    info_box.classList.remove('activeInfo');
-}
 
-continue_btn.onclick = ()=> {
-    info_box.classList.remove('activeInfo');
-    quiz_box.classList.add('activeQuiz');
-    showQuestions(0);
-}
 
-let que_count = 0;
+// const start_btn = document.querySelector(".start_btn button");
+// const info_box = document.querySelector(".info_box");
+// const exit_btn = info_box.querySelector(".buttons .quit");
+// const continue_btn = info_box.querySelector(".buttons .restart");
+// const quiz_box = document.querySelector(".quiz_box");
 
-function showQuestions(index) {
-    const que_text = document.querySelector(".que_text");
-    const option_list = document.querySelector(".option_list");
-    let que_tag = '<span>' + questions[index].question + '</span>';
-    // let option_tag = '<div class = "option">' + questions[index].options[0] + '<span></span></dic>'
-    //                  + '<div class = "option">'+ questions[index].options[1] + '<span></span></dic>'
-    //                  + '<div class = "option">'+ questions[index].options[2] + '<span></span></dic>'
-    //                  + '<div class = "option">'+ questions[index].options[3] + '<span></span></dic>'
-    que_text.innerHTML = que_tag;
-    option_list.innerHTML = option_tag;
-}
+
+// start_btn.onclick = ()=> {
+//     info_box.classList.add('activeInfo');
+// }
+
+// exit_btn.onclick = ()=> {
+//     info_box.classList.remove('activeInfo');
+// }
+
+// continue_btn.onclick = ()=> {
+//     info_box.classList.remove('activeInfo');
+//     quiz_box.classList.add('activeQuiz');
+//     showQuestions(0);
+// }
+
+// let que_count = 0;
+
+// function showQuestions(index) {
+//     const que_text = document.querySelector(".que_text");
+//     const option_list = document.querySelector(".option_list");
+//     let que_tag = '<span>' + questions[index].question + '</span>';
+//     // let option_tag = '<div class = "option">' + questions[index].options[0] + '<span></span></dic>'
+//     //                  + '<div class = "option">'+ questions[index].options[1] + '<span></span></dic>'
+//     //                  + '<div class = "option">'+ questions[index].options[2] + '<span></span></dic>'
+//     //                  + '<div class = "option">'+ questions[index].options[3] + '<span></span></dic>'
+//     que_text.innerHTML = que_tag;
+//     option_list.innerHTML = option_tag;
+// }
+
+
